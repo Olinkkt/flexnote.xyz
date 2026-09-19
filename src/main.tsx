d7284cfa336a1a7dd316d6ff93155d44907de013
Oliver Seidl <seidl.oliver@icloud.com>
2026-09-19 02:24:10 +0200

    feat: add @vercel/analytics and @vercel/speed-insights

--- src/main.tsx
+++ src/main.tsx
@@ -29,11 +29,15 @@ if ('serviceWorker' in navigator && !window.location.host.includes('localhost:51
 }
 
 import { ErrorBoundary } from './components/ErrorBoundary';
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
       <App />
+      <Analytics />
+      <SpeedInsights />
     </ErrorBoundary>
   </React.StrictMode>,
 );
